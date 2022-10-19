25ec595c28996847e0591dc669e505c3ec455d23
cho-in <chrndlek@gmail.com>
2022-10-19 13:22:05 +0900

    Example

--- Example/src/Management/ManagementMain.java
+++ Example/src/Management/ManagementMain.java
@@ -3,15 +3,33 @@ package Management;
 public class ManagementMain {
 
     public static void main(String[] args) {
+        //
+        // ** 맴버 객체를 생성한다. 객체화 된것을 인스턴스(추상적)라고 한다(객체가 아님.). 객체는 실물화 된것.
         Member member = new Member();
+
+        // ** 생성한 멤버 개체에 필드값을 셋팅한다.
         member.setAge(32);
         member.setUserName("홍길동");
 
+        // ** 멤버 객체와 has-a 관계인 Group class 를 생성한다. ★ 중요. 가지게 된것. is a
+        Group group = new Group();
+        // Group group = new Group();
 
+        // ** 생성된 클래스의 필드값을 셋팅한다.
+        group.set_class("K");
+        group.setSubject("Java");
+
+        // ** 값이 셋팅된 클래스를 멤버 클래스에 맵핑한다.
+        member.setGroup(group);  // 그룹 값을 저장.
+
+        // ** 멤버의 필드값을 받아온다.
         String name = member.getUserName();
         int age = member.getAge();
+        String _class = member.getGroup().get_class();
+        String subject= member.getGroup().getSubject();
 
-        System.out.println(name + " : " + age);
+        // ** 출력한다.
+        System.out.println(name + " : " + age + " [ " + _class+ " ] :" + subject);
 
     }
 
--- Example/src/Management/Member.java
+++ Example/src/Management/Member.java
@@ -3,11 +3,15 @@ package Management;
 import javax.swing.*;
 
 public class Member extends Parent {
-    int age;
+    int age;        // is a
 
     String userName;
 
-    Group maga;
+    Group group;  // has a 가지게 된것 상속이 아님.
+
+    public Group getGroup() {return group;}
+
+    public void setGroup(Group group) {this.group = group;}
 
     public int getAge() { return this.age;}
     public void setAge(int age) {this.age = age;}
