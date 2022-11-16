0380d87cf7e68a44c3339e67d69e17155975ea10
cho-in <chrndlek@gmail.com>
2022-11-16 13:22:07 +0900

    demo4

---
+++ demo4/src/main/java/com/example/demo/relation/Member.java
@@ -0,0 +1,29 @@
+package com.example.demo.relation;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor  // 방향성은 객체 상태 일때에만 존재한다.
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(name = "member_name")
+    private String memberName;
+
+    @JoinColumn
+    @ManyToOne
+    private Academy academy;
+
+    public Member(String memberName, Academy academy)
+    {
+        this.memberName = memberName;
+        this.academy = academy;
+    }
+}
